28a154e5cef880be4f6c54ab788230d355d5fcf4
DESKTOP-RKRBSM6\Umar <umarimanmagzam@gmail.com>
2025-07-05 15:57:45 +0500

    Added adapt for any devices

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -11,29 +11,29 @@ function Header({ money }: HeaderProps) {
         <div className="header__banner">
           <div className="header__banner-inner container">
             <a className="header__banner-logo" href="/">
-              <h1 className="h2">Spend $420.000.000.000</h1>
+              <h1 className="header__banner-title">Spend $420.000.000.000</h1>
             </a>
             <select name="" id=""></select>
           </div>
         </div>
         <div className="header__body">
-          <img src={Elon} alt="Elon Mask" />
-          <h2 className="header__title h3">Spend Elon Musk's Money</h2>
-          <p className="header__description ">
+          <img className='header__body-image' src={Elon} alt="Elon Mask" />
+          <h2 className="header__body-title h3">Spend Elon Musk's Money</h2>
+          <p className="header__body-description ">
             If Elon cashed out all of his stocks & assets today he would have
             approximately $420,000,000,000 (US Dollars) in his bank account
             (Forbes 2025).
           </p>
-          <p className="header__description ">¿What would you spend it on?</p>
-          <p className="header__description ">¡Have your receipt at the end!</p>
+          <p className="header__body-description ">¿What would you spend it on?</p>
+          <p className="header__body-description ">¡Have your receipt at the end!</p>
         </div>
       </header>
       <div className="header__progress">
         <div className="header__progress-inner container">
-          <p className="header__progress-count h2">
+          <p className="header__progress-count">
             Remaining: $ {money.toLocaleString()}{" "}
           </p>
-          <p className="header__progress-percentage h2">You spent:</p>
+          <p className="header__progress-percentage">You spent:</p>
         </div>
       </div>
     </>
